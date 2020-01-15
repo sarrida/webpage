41d51657489829d883e2b351c72b65b2d09927e8
sarrida <sarridahm@gmail.com>
2020-01-15 13:13:45 +0700

    web

---
+++ footer.php
@@ -0,0 +1,66 @@
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <!-- <div class="row mb-12">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-9">
+              <h2 class="ftco-heading-2">TTMED.</h2>
+              <p>สำนักงานคณะการแพทย์แผนไทย มหาวิทยาลัยสงขลานครินทร์
+              อาคารคณะการจัดการสิ่งแวดล้อม ชั้น 8
+              มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตหาดใหญ่
+              15 ถ.กาญจนวณิชย์ อ.หาดใหญ่ จ.สงขลา 90110
+               </p>
+              <p>
+               โรงพยาบาลการแพทย์แผนไทย มหาวิทยาลัยสงขลานครินทร์
+              อาคารคาเฟต ติดกับอาคารศูนย์กีฬา
+              มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตหาดใหญ่
+              15 ถ.กาญจนวณิชย์ อ.หาดใหญ่ จ.สงขลา 90110</p>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-6">
+              <h2 class="ftco-heading-2"></h2>
+              <ul class="list-unstyled">
+                <li><a href="#" class="py-2 d-block">Home</a></li>
+                <li><a href="#" class="py-2 d-block">Case studies</a></li>
+                <li><a href="#" class="py-2 d-block">Services</a></li>
+                <li><a href="#" class="py-2 d-block">Portfolio</a></li>
+                <li><a href="#" class="py-2 d-block">About</a></li>
+                <li><a href="#" class="py-2 d-block">Contact</a></li>
+              </ul>
+              <p>
+               โรงพยาบาลการแพทย์แผนไทย มหาวิทยาลัยสงขลานครินทร์
+              อาคารคาเฟต ติดกับอาคารศูนย์กีฬา
+              มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตหาดใหญ่
+              15 ถ.กาญจนวณิชย์ อ.หาดใหญ่ จ.สงขลา 90110</p>
+            </div>
+          </div>
+          <div class="col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Contact Information</h2>
+              <ul class="list-unstyled">
+                <li><a href="#" class="py-2 d-block">198 West 21th Street, Suite 721 New York NY 10016</a></li>
+                <li><a href="#" class="py-2 d-block">+ 1235 2355 98</a></li>
+                <li><a href="#" class="py-2 d-block">info@yoursite.com</a></li>
+                <li><a href="#" class="py-2 d-block">email@email.com</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+        </div> -->
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Faculty of Traditional Thai Medicine <i class="icon-heart" aria-hidden="true"></i></p>
+            <p>building Faculty of Environmental Management, 8th floor Tel.(+66) 0-742-82701-9</p>
+          </div>
+        </div>
+      </div>
+    </footer>
---
+++ navbar.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">ttmed.</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="oi oi-menu"></span> Menu
+      </button>
+
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About Us</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown04">
+                <a class="dropdown-item" href="">History / Vision</a>
+                <a class="dropdown-item" href="">Faculty Emblem, Flower and Color</a>
+              </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Program</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown05">
+                <a class="dropdown-item" href="">Undergraduate Programs</a>
+                <a class="dropdown-item" href="">Graduate Programs</a>
+                <a class="dropdown-item" href="">Research</a>
+                <a class="dropdown-item" href="">Scholarship</a>
+              </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="" id="dropdown06" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Departments & Units</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown06">
+                <a class="dropdown-item" href="">Thai Medicine</a>
+                <a class="dropdown-item" href="">Thai Drugs</a>
+                <a class="dropdown-item" href="">Thai Massage & Thai Midwifery</a>
+                <a class="dropdown-item" href="">Traditional Thai Medicine Hospital</a>
+              </div>
+          </li>
+          <li class="nav-item"><a href="blog.html" class="nav-link">Staff</a></li>
+          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+          <li class="nav-item cta">
+            <a href="http://www.ttmed.psu.ac.th/" target="_self" class="nav-link"><span><img src="images/th.gif" border="0"> &nbsp; TH</span></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+</nav>
