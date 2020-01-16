9c6a6215cd5d10e3d05786952c1252f4e6fd08f0
nillaya <5920610002@psu.ac.th>
2020-01-16 10:35:10 +0700

    history

--- history.php
+++ history.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <title>Snipp - Free Bootstrap 4 Template by Colorlib</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+
     <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,700,800" rel="stylesheet">
 
     <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
     <link rel="stylesheet" href="css/animate.css">
-    
+
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/magnific-popup.css">
@@ -21,71 +22,177 @@
     <link rel="stylesheet" href="css/bootstrap-datepicker.css">
     <link rel="stylesheet" href="css/jquery.timepicker.css">
 
-    
+
     <link rel="stylesheet" href="css/flaticon.css">
     <link rel="stylesheet" href="css/icomoon.css">
     <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body>
-   <!-- navbar -->
-   <?php include('navbar.php')?>
+</head>
+
+<body>
+    <!-- navbar -->
+    <?php include('navbar.php') ?>
     <!-- END nav -->
-    
+
     <div class="hero-wrap js-fullheight">
-      <div class="overlay"></div>
-      <div id="particles-js"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
-          <div class="col-md-6 ftco-animate text-center" data-scrollax=" properties: { translateY: '70%' }">
-            <p class="breadcrumbs" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span class="mr-2"><a href="index.php">Home</a></span> <span>History and Vision</span></p>
-            <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">History and Vision</h1>
-          </div>
+        <div class="overlay"></div>
+        <div id="particles-js"></div>
+        <div class="container">
+            <div class="row no-gutters slider-text align-items-center justify-content-center"
+                data-scrollax-parent="true">
+                <div class="col-md-6 ftco-animate text-center" data-scrollax=" properties: { translateY: '70%' }">
+                    <p class="breadcrumbs" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span
+                            class="mr-2"><a href="index.php">Home</a></span> <span>History and Vision</span></p>
+                    <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">History and
+                        Vision</h1>
+
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-    
+
     <section class="ftco-section contact-section ftco-degree-bg">
-      <div class="container bg-light">
-        <div class="row d-flex mb-5 contact-info">
-          <div class="col-md-12 mb-4">
-            <h2 class="h4">History and Vision</h2>
-          </div>
-          
-        </div>
-      </div>
+        <div class="container bg-light">
+            <div class="row d-flex mb-5 contact-info">
+                <div class="col-md-12 mb-4">
+                    <h2 class="h4">History and Vision</h2>
+
+                    <p style="text-indent:2.5em;"> The Faculty of Traditional Thai Medicine, PSU intends to be the
+                        leading, preeminent teaching and
+                        research
+                        institution with focus on the application of the knowledge in the field of Thai traditional
+                        medicine for Thai
+                        society and the world at large.
+                    </p>
+                </div>
+
+                <div class="col-md-12 mb-4">
+                    <h2 class="h4">History of Faculty of Traditional Thai Medicine</h2>
+
+                    <p style="text-indent:2.5em;">Thai Traditional Medicine has coexisted with Thai society for
+                        centuries.
+                        According to
+                        the
+                        history of
+                        Thailand, there was some evidence appearing in the era of Sukhothai Kingdom that this kind of
+                        medicine
+                        was founded on practical experiences in Thai’s knowledge and wisdom accumulated and transferred
+                        through
+                        the ages. Although Thai Traditional Medicine has been neglected and replaced by conventional
+                        medicine,
+                        it has not totally forgotten by the Thai society since Thai Traditional Medicine is needed for
+                        curing
+                        diseases and maintaining good health. In addition, the study and techniques of Thai traditional
+                        Medicine
+                        was individually transferred by practitioners, through private organizations and in associations
+                        or
+                        groups involved with Thai Traditional Medicine and Thai pharmacy. </p>
+                    <p style="text-indent:2.5em;"> Prince of Songkla University (PSU), a leading university in the South
+                        of Thailand,
+                        has a
+                        resolution
+                        to be a supporter of the society. Furthermore, PSU recognizes the importance of playing active
+                        role
+                        in Thai Traditional Medicine instruction for higher education in order to preserve, study,
+                        research,
+                        and develop its benefits to the society and the nation. The curriculum of Thai Traditional
+                        Medicine
+                        was thus incorporated into PSU development plan (No.9) and initiated in order to set up the
+                        Bachelor
+                        Degree of Science program in Thai Traditional Medicine.
+
+                    </p>
+                </div>
+                <div class="col-md-12 mb-4">
+                    <h2 class="h4">Significance</h2>
+
+                    <p style="text-indent:2.5em;"> Thai Traditional Medicine is one of aspect of medical sciences that
+                        support, prevent, cure, and rehabilitate health by means of homeostasis. Thai Traditional
+                        Medicine also emphasizes on holistic health care in terms of the combination of body, mind and
+                        society; hence, Thai Traditional Medicine is different from conventional medicine. For the
+                        aforementioned reasons, more people opt for Thai Traditional Medicine as a method of health
+                        care. Additionally, Thai Traditional Medicine is a reservoir of accumulated wisdom since the
+                        ancient time. However, scientific and academic evidences are inadequate to support the acclaimed
+                        wisdom.
+                    </p>
+                    <p style="text-indent:2.5em;"> Prince of Songkla University, a Research oriented University, has
+                        instituted a university’s development plan in order to enhance research in terms of personnel,
+                        fundamental structure, research scholarships, and research contributions. Faculty of Traditional
+                        Thai Medicine, Prince of Songkla University has also recognizes the necessity in developing its
+                        role to be a research faculty in order to be relevant to the university’s development plan. The
+                        Faculty of Traditional Thai Medicine aims at developing researchers and academics who have the
+                        ability to conduct high level of research, create research works in a field of Thai Traditional
+                        Medicine and Folk Medicine, as well as develop and apply new knowledge that conforms with the
+                        society and nation’s needs. As such, the curriculum of the Doctor of Philosophy in Thai
+                        Traditional Medicine Program was created in order to develop higher level of research works in
+                        the field of Thai Traditional Medicine, and respond to the university and faculty’s development
+                        plans. This curriculum is intended to produce graduates who have knowledge and advanced
+                        expertise in a field of Thai Traditional Medicine, create quality research works accepted
+                        nationally and internationally, as well as create new knowledge which can be further applied to
+                        the field of Thai Traditional Medicine.
+                    </p>
+                </div>
+                <div class="col-md-12 mb-4">
+                    <h2 class="h4">Objectives</h2>
+
+                    <li>To deeply enlarge the knowledge of Thai Traditional Medicine</li>
+                    <li>To be able to develop research work and create new knowledge accepted nationally and
+                        internationally</li>
+                    <li>To have academic leadership and the ability in applying the knowledge of Thai Traditional
+                        Medicine for the benefit of the society</li>
+                    <li>To possess high morality and academic ethics</li>
+                    <li> To have possess critical thinking skills and applications of technology in the development
+                        of efficent Thai Traditional Medicine &nbsp;&nbsp;&nbsp;&nbsp;research</li>
+                </div>
+
+                <div class="col-md-12 mb-4">
+                    <h2 class="h4">Academic services</h2>
+                    <p style="text-indent:2.5em;">
+                        <p style="text-indent:2.5em;"> The services provided for the general public include both
+                            academic and professional ones in health promotion, disease prevention and treatment and
+                            rehabilitation based on the principles of Thai traditional medicine. We also provide free
+                            academic services to the society and offer various TTM courses (Thai massage, formulation of
+                            herbal inhaler, etc.) to the public. In addition, we also assist in conducting research
+                            activities as well as create innovative projects for both the government, private agencies
+                            and community enterprise.</p>
+
+                </div>
+            </div>
     </section>
-    
+
     <!-- footer -->
-    <?php include('footer.php')?>
+    <?php include('footer.php') ?>
     <!-- end footer -->
-  <!-- loader -->
-  <div id="ftco-loader" class="show fullscreen">
-    <svg class="circular" width="48px" height="48px">
-      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/>
-      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/>
-    </svg>
-  </div>
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen">
+        <svg class="circular" width="48px" height="48px">
+            <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
+                stroke="#F96D00" />
+        </svg>
+    </div>
 
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/jquery.waypoints.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.animateNumber.min.js"></script>
-  <script src="js/bootstrap-datepicker.js"></script>
-  <script src="js/jquery.timepicker.min.js"></script>
-  <script src="js/particles.min.js"></script>
-  <script src="js/particle.js"></script>
-  <script src="js/scrollax.min.js"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="js/google-map.js"></script>
-  <script src="js/main.js"></script>
-    
-  </body>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
+    <script src="js/jquery.timepicker.min.js"></script>
+    <script src="js/particles.min.js"></script>
+    <script src="js/particle.js"></script>
+    <script src="js/scrollax.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false">
+    </script>
+    <script src="js/google-map.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
 </html>
